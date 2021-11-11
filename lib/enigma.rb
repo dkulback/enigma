fc63f7c2e9654a688b74eaf33f45c6ac43d4f673
Darren Kulback <dkulback@gmail.com>
2021-11-10 22:43:58 -0700

    Add tests according to random key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,25 @@
 class Enigma
-  attr_reader :key_generator
-  def initialize(key_generator: GenerateKey)
-    @key_generator = key_generator
+  attr_reader :key_gen
+
+  def initialize(key_gen: KeyGen)
+    @keygen = keygen
   end
-  def encrypt(message, key = key_generator.call, date = Date.today)
-      encrypted =
+
+  def encrypt(message, key = key_gen.call, date = Date.today)
+    encrypted =
       {
         encryption: message,
         key: key,
         date: date
       }
+  end
+
+  def decrypt(message, key = key_gen.call, date = Date.today)
+    encrypted =
+      {
+        encryption: message,
+        key: key,
+        date: date
+      }
+  end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,7 +3,19 @@ SimpleCov.start
 require './lib/enigma'
 
 RSpec.describe Enigma do
-  let(:enigma) { Enigma.new }
+  class MockGenKey
+    def initialize(key)
+      @key = key
+    end
+
+    def call
+      @key
+    end
+  end
+  let(:mock_key) { '01234' }
+  let(:mock_generate_key) { MockGenKey.new(mock_key) }
+  let(:enigma) { Enigma.new(key_generator: mock_generate_key) }
+
   it 'exists' do
     actual = enigma
     expected = Enigma
@@ -11,67 +23,81 @@ RSpec.describe Enigma do
   end
 
   describe '#encrypt/3' do
-    it 'encrypts messages with key and date' do
-      actual = enigma.encrypt("hello world", "02715", "040895")
+    xit 'encrypts messages with key and date' do
+      actual = enigma.encrypt('hello world', '02715', '040895')
       expected =
-      {
-        encryption: "keder ohulw",
-        key: "02715",
-        date: "040895"
-      }
+        {
+          encryption: 'keder ohulw',
+          key: '02715',
+          date: '040895'
+        }
       expect(actual).to eq(expected)
+    end
+  end
+  describe 'encrypt/2' do
+    before do
+      Timecop.freeze_at('1995-04-08')
     end
     xit 'encrypts messages with key and uses todays date' do
-      actual = enigma.encrypt("hello world", "02715")
+      actual = enigma.encrypt('hello world', '02715')
       expected =
-      {
-        encryption: "keder ohulw",
-        key: "02715",
-        date: Date.today
-      }
+        {
+          encryption: 'keder ohulw',
+          key: '02715',
+          date: Date.today
+        }
       expect(actual).to eq(expected)
     end
-    xit 'encrypts messages by generating a key and uses today date' do
-      actual = enigma.encrypt("hello world")
+  end
+  describe 'encrypt/1' do
+    before do
+      Timecop.freeze_at('1995-04-08')
+    end
+    it 'encrypts messages by generating a key and uses today date' do
+      actual = enigma.encrypt('hello world')
       expected =
-      {
-        encryption: "keder ohulw",
-        key: GenerateKey.new,
-        date: Date.today
-      }
+        {
+          encryption: 'keder ohulw',
+          key: mock_key,
+          date: Date.today
+        }
       expect(actual).to eq(expected)
     end
   end
   describe '#decrypt/3' do
     xit 'decrypts messages' do
-      actual = enigma.decrypt("keder ohulw", "02715", "040895")
+      actual = enigma.decrypt('keder ohulw', '02715', '040895')
       expected =
-      {
-        encryption: "hello world",
-        key: "02715",
-        date: "040895"
-      }
+        {
+          encryption: 'hello world',
+          key: '02715',
+          date: '040895'
+        }
       expect(actual).to eq(expected)
     end
-    xit 'decrypts messages with key and uses todays date' do
-      actual = enigma.decrypt("keder ohulw", "02715")
-      expected =
-      {
-        encryption: "hello world",
-        key: "02715",
-        date: Date.today
-      }
-      expect(actual).to eq(expected)
+    describe '#decrypt/2' do
+      xit 'decrypts messages with key and uses todays date' do
+        actual = enigma.decrypt('keder ohulw', '02715')
+        expected =
+          {
+            encryption: 'hello world',
+            key: '02715',
+            date: Date.today
+          }
+        expect(actual).to eq(expected)
+      end
     end
-    xit 'decrypts messages by generating a key and uses today date' do
-      actual = enigma.decrypt("keder ohulw", "02715", "040895")
-      expected =
-      {
-        encryption: "hello world",
-        key: GenerateKey.new,
-        date: Date.today
-      }
-      expect(actual).to eq(expected)
+    describe '#decrypt/1' do
+      xit 'decrypts messages by generating a key and uses today date' do
+        actual = enigma.decrypt('keder ohulw')
+        expected =
+          {
+            encryption: 'hello world',
+            key: GenKey.new,
+            date: Date.today
+          }
+        expect(actual).to eq(expected)
+      end
     end
   end
 end
