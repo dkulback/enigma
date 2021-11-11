481f7e2e0d5ea9c3adb8962090f60ff89f6c9b4b
Darren Kulback <dkulback@gmail.com>
2021-11-11 14:05:30 -0700

    Add combined method

--- spec/scrambler_spec.rb
+++ spec/scrambler_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Scrambler do
     expect(actual).to be_a(Scrambler)
   end
   describe '#keys/1' do
-    it 'creates a 4 part key values' do
+    it 'creates a 4 step key values with injected key' do
       actual = scrambler.keys('02715')
       expected =
         { A: 2,
@@ -24,9 +24,36 @@ RSpec.describe Scrambler do
     end
   end
   describe '#shifts/1' do
-    it 'creates offset values' do
+    it 'creates offset values with date' do
       actual = scrambler.shifts('040889')
-      expected = [1, 7, 6, 1]
+      expected = {
+        A:0,
+        B:3,
+        C:2,
+        D:1
+      }
+      expect(actual).to eq(expected)
+    end
+    it 'creates offset values using todays date' do
+      actual = scrambler.shifts
+      expected = {
+        A:6,
+        B:6,
+        C:4,
+        D:1
+      }
+      expect(actual).to eq(expected)
+    end
+  end
+  describe '#combined/2' do
+    it 'combines the key postion with the shifts' do
+      actual = scrambler.combined('02715', '040889')
+      expected =
+      { A: 2,
+        B: 30,
+        C: 73,
+        D: 16
+       }
       expect(actual).to eq(expected)
     end
   end
