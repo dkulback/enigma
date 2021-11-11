2a88354b7d88b851627d3b42e4c15fea46196760
Darren Kulback <dkulback@gmail.com>
2021-11-11 11:40:03 -0700

    Add scrambler shift method

--- lib/scrambler.rb
+++ lib/scrambler.rb
@@ -1,7 +1,19 @@
 class Scrambler
-  def keys(_key_gen)
-    scramble = {}
+
+  def keys_shifts(key, date = Date.today.strftime('%d%m%y'))
+    keys =
+      {
+        A: key[0..1].to_i,
+        B: key[1..2].to_i,
+        C: key[2..3].to_i,
+        D: key[3..4].to_i
+      }
+    phase_1 = date.to_i * date.to_i
+    phase_2 = phase_1.digits[-4, 4]
+  end
+
+  def shifts(date = Date.today.strftime('%d%m%y'))
+    phase_1 = date.to_i * date.to_i
+    phase_2 = phase_1.digits[-4, 4]
   end
 end
-require 'pry'
-binding.pry
--- spec/scrambler_spec.rb
+++ spec/scrambler_spec.rb
@@ -12,14 +12,23 @@ RSpec.describe Scrambler do
     expect(actual).to be_a(Scrambler)
   end
   describe '#keys/1' do
-    it 'creates a 4 part keys scramble/1' do
-      actual = scrambler.scrambilze(KeyGen.generate)
+    it 'creates a 4 part key values' do
+      actual = scrambler.keys('02715')
       expected =
-        { A: 12,
-          B: 23,
-          C: 34,
-          D: 45 }
-      expect(actual).to eq(Scrambler)
+        { A: 2,
+          B: 27,
+          C: 71,
+          D: 15
+         }
+      expect(actual).to eq(expected)
     end
   end
+  describe '#shifts/1' do
+    it 'creates offset values' do
+      actual = scrambler.shifts('040889')
+      expected = [1, 7, 6, 1]
+      expect(actual).to eq(expected)
+    end
+  end
+
 end
