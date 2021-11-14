39000822ad9c68da8774c0edfcd397bdde72094a
Darren Kulback <dkulback@gmail.com>
2021-11-13 22:05:40 -0700

    Add fileIO iteration

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,1 +1,10 @@
 require_relative 'enigma'
+require_relative 'keygen'
+
+handle = File.open(ARGV[0], 'r')
+
+incoming_text = handle.read.strip
+handle.close
+encrypted_text = Enigma.new.encrypt(incoming_text)
+
+puts encrypted_text
